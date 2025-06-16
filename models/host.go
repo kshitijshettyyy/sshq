c30b74d2c3dfc5837d1f8c689d1703ffb1a9f498
Kshitij Shetty <kshitijshetty@ibm.com>
2025-06-16 10:24:16 +0530

    Added comments to models

--- models/host.go
+++ models/host.go
@@ -1,5 +1,17 @@
 package models
 
+
+/*
+// this is the format in which the host configs are stored in the json.
+example:
+{
+User root
+Address 9.46.111.218
+Port 22
+}
+
+The prot for our ssh connections is 22 by default. Need to default it in future.
+*/
 type Host struct {
 	User    string `json:"user"`
 	Address string `json:"address"`
